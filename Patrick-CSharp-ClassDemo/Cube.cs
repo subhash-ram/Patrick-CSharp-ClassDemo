dcf3ddcd85364219794afea844a1b2c6c6ff6ea2
Subhash Ramachandran <subhash.ram@outlook.com>
2022-05-20 15:15:06 -0500

    no space

--- Patrick-CSharp-ClassDemo/Cube.cs
+++ Patrick-CSharp-ClassDemo/Cube.cs
@@ -5,7 +5,6 @@ namespace Patrick_CSharp_ClassDemo
         public override int CalculateArea(int length)
         {
             return base.CalculateArea(length) * 6;
-            
         }
     }
 }
